3524809ae0116c002c365a6afdf7b4a3fef35d7e
tobilarscheid <tobilarscheid@gmail.com>
2016-11-22 12:35:56 +0100

    by default ignore etag in json or xml

--- src/main/java/de/tobiaslarscheid/etag/interfaces/ETagged.java
+++ src/main/java/de/tobiaslarscheid/etag/interfaces/ETagged.java
@@ -1,5 +1,11 @@
 package de.tobiaslarscheid.etag.interfaces;
 
+import javax.xml.bind.annotation.XmlTransient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface ETagged {
+	@XmlTransient
+	@JsonIgnore
 	public String getETag();
 }
